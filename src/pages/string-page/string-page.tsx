6a193893efa23484a31a2fc2710f420be19844fa
Igor Petrov <galdenysh@MacBook-Air-Igor.local>
2022-09-12 11:01:21 +0300

    feat: add string styles

--- src/pages/string-page/string-page.tsx
+++ src/pages/string-page/string-page.tsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { Button } from "../../components/ui/button/button";
+import { Circle } from "../../components/ui/circle/circle";
+import { Input } from "../../components/ui/input/input";
 import { SolutionLayout } from "../../components/ui/solution-layout/solution-layout";
+import styles from "./string-page.module.css";
 
 export const StringComponent: React.FC = () => {
-  return <SolutionLayout title="Строка"></SolutionLayout>;
+  const [value, setValue] = useState("");
+  const [arr, setArr] = useState<string[]>([]);
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    setArr(value.split(""));
+  };
+
+  const onChangeInput = (evt: ChangeEvent<HTMLInputElement>) => {
+    setValue(evt.target.value);
+  };
+
+  return (
+    <SolutionLayout title="Строка">
+      <form className={styles.inputWrap} onSubmit={handleSubmit}>
+        <Input extraClass={styles.input} isLimitText={true} maxLength={11} value={value} onChange={onChangeInput} />
+        <Button type="submit" text="Развернуть" />
+      </form>
+      <div className={styles.circleWrap}>
+        {arr.map((item, index) => (
+          <Circle extraClass={styles.circle} key={index} letter={item} />
+        ))}
+      </div>
+    </SolutionLayout>
+  );
 };
