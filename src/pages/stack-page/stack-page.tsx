05ccf603601ffd73ebb8373469e1581da9d510d5
Igor Petrov <galdenysh@air-igor.jugru.org>
2022-09-13 16:00:42 +0300

    feat: add stack logic

--- src/pages/stack-page/stack-page.tsx
+++ src/pages/stack-page/stack-page.tsx
@@ -4,52 +4,63 @@ import { Circle } from "../../components/ui/circle/circle";
 import { Input } from "../../components/ui/input/input";
 import { SolutionLayout } from "../../components/ui/solution-layout/solution-layout";
 import { ElementStates } from "../../types/element-states";
+import { delay } from "../../utils/funcs";
+import stack from "../../utils/stack";
 import styles from "./stack-page.module.css";
 
 export const StackPage: React.FC = () => {
   const [value, setValue] = useState("");
-  const [arr, setArr] = useState<string[]>(["123"]);
-  const [currentIdx, setCurrentIdx] = useState<Number[]>([]);
-  const [sortedIdx, setSortedIdx] = useState<Number[]>([]);
+  const [arr, setArr] = useState<unknown[]>([]);
   const [pending, setPending] = useState(false);
 
-  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-
     const submitEvent = evt.nativeEvent as SubmitEvent;
 
-    console.log(submitEvent.submitter?.getAttribute("value"));
+    setPending(true);
+
+    if (submitEvent.submitter?.getAttribute("value") === "add") {
+      stack.push(value);
+      setArr(stack.elements());
+      setValue("");
+    }
+    if (submitEvent.submitter?.getAttribute("value") === "remove") {
+      stack.pop();
+      setArr(stack.elements());
+    }
+
+    await delay(500);
+    setPending(false);
   };
 
   const onChangeInput = (evt: ChangeEvent<HTMLInputElement>) => {
     setValue(evt.target.value);
   };
 
+  const handleRemoveBtn = () => {
+    stack.clear();
+    setArr(stack.elements());
+  };
+
   return (
     <SolutionLayout title="Стек">
       <div className={styles.container}>
         <form className={styles.inputWrap} onSubmit={handleSubmit}>
-          <Input extraClass={styles.input} isLimitText={true} maxLength={4} value={value} onChange={onChangeInput} disabled={pending} />
-          <Button extraClass={styles.btn} type="submit" value="add" text="Добавить" disabled={pending} isLoader={pending} />
-          <Button extraClass={styles.btn} type="submit" value="remove" text="Удалить" disabled={pending} isLoader={pending} />
+          <Input extraClass={styles.input} isLimitText={true} maxLength={4} value={value} onChange={onChangeInput} />
+          <Button extraClass={styles.btn} type="submit" value="add" text="Добавить" disabled={value === ""} />
+          <Button extraClass={styles.btn} type="submit" value="remove" text="Удалить" disabled={stack.size() === 0} />
         </form>
-        <Button extraClass={styles.btn} type="button" text="Отчистить" disabled={pending} />
+        <Button extraClass={styles.btn} type="button" text="Отчистить" onClick={handleRemoveBtn} disabled={stack.size() === 0} />
       </div>
       <div className={styles.circleWrap}>
         {arr.map((item, index) => (
           <Circle
             extraClass={styles.circle}
             key={index}
-            letter={item}
-            head={"head"}
-            tail={"tail"}
-            state={
-              sortedIdx.includes(index)
-                ? ElementStates.Modified
-                : currentIdx.includes(index)
-                ? ElementStates.Changing
-                : ElementStates.Default
-            }
+            letter={String(item)}
+            head={index === stack.size() - 1 ? "top" : ""}
+            tail={String(index)}
+            state={index === stack.size() - 1 && pending ? ElementStates.Changing : ElementStates.Default}
           />
         ))}
       </div>
