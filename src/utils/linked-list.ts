14d0ad45032033030d23aba82f4dec5e8ee9e825
Igor Petrov <galdenysh@air-igor.jugru.org>
2022-09-15 21:12:58 +0300

    feat: add prepend

--- src/utils/linked-list.ts
+++ src/utils/linked-list.ts
@@ -1,16 +1,22 @@
 import { LinkedListNode } from "./linked-list-node";
 
 class LinkedList<T> {
-  private store: T[];
+  // private store: T[];
   private head: LinkedListNode<T> | null = null;
 
-  constructor(list: T[]) {
-    this.store = list;
-  }
+  // constructor(list: T[]) {
+  //   this.store = list;
+  // }
 
   prepend(item: T) {
     const node = new LinkedListNode(item);
-    this.store = [node.item, ...this.store];
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      node.nextItem = this.head;
+      this.head = node;
+    }
   }
   append(item: T) {
     const node = new LinkedListNode(item);
@@ -28,16 +34,16 @@ class LinkedList<T> {
     }
   }
   addByIndex(index: number, item: T) {
-    this.store.splice(index, 0, item);
+    // this.store.splice(index, 0, item);
   }
   deleteByIndex(index: number) {
-    this.store.splice(index, 1);
+    // this.store.splice(index, 1);
   }
   deleteHead() {
-    this.store.splice(0, 1);
+    // this.store.splice(0, 1);
   }
   deleteTail() {
-    this.store.splice(this.store.length - 1, 1);
+    // this.store.splice(this.store.length - 1, 1);
   }
   clear() {
     this.head = null;
@@ -55,4 +61,4 @@ class LinkedList<T> {
   }
 }
 
-export default new LinkedList(["0", "34", "8", "1"]);
+export default new LinkedList();
